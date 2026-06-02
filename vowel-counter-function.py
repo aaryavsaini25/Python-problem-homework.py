1ac819e3fa8dad06156b31afd7dbdadd28e2c669
Aaryav <aaryav.saini25@gmail.com>
2026-06-02 20:14:50 +0100

    Finished the sixth function!

--- vowel-counter-function.py
+++ vowel-counter-function.py
@@ -1,1 +1,8 @@
-p=''
+global text
+text=str
+def count_vowels(text):
+    text=input("Enter a word or sentence:")
+    vowels='aeiou'
+    vowel_count = sum(text.count(v) for v in vowels)
+    print(f"The word/sentence {text} has {vowel_count} vowels!")
+count_vowels(text)
